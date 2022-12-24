3fc4bb91e7b46c556855a03ad29def17326609e4
Ivan <ivan.inclan.327b@gmail.com>
2022-12-23 21:28:02 -0600

    Se añadieron los cors para permitir los endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
-import express from 'express'; 
+const cors = require('cors');
 
+import express from 'express'; 
 import parkingRouter from './routes/parking';
 
 
 const app = express();
 app.use(express.json()) //middleware que transforma la req.body a un json
 
-const PORT = 3000;
+const PORT = 5000;
+
+app.use(cors({
+    origin: ['http://localhost:3000'],
+    methods: ['GET', 'POST', 'DELETE', 'UPDATE']
+}));
 
 app.use('/api/parkings', parkingRouter);
 
